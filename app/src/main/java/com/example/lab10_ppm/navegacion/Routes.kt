93348572bb656f4b01ddf5bb2d3992e9b60d50f6
JSPA <per22318@uvg.edu.gt>
2023-10-31 08:37:56 -0600

    cambios lab 11

--- app/src/main/java/com/example/lab10_ppm/navegacion/Routes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lab10_ppm.navegacion
-
-sealed class Routes(val route: String) {
-    object Login: Routes("Login")
-    object main: Routes("Main")
-    object Cusuario: Routes("Cusuario")
-    object ForgotPassword: Routes("ForgotPassword")
-}
---
+++ app/src/main/java/com/example/lab11_ppm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab11_ppm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab10_ppm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
